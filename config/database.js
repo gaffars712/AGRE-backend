88249ae129cd0499c76d44a33dbef94c0fe47e72
ShaikhGaffar <134927774+gaffars712@users.noreply.github.com>
2024-06-26 10:16:33 +0530

    update

--- config/database.js
+++ config/database.js
@@ -28,9 +28,9 @@ module.exports = ({ env }) => {
     // },
     mysql2: {
       connection: {
-        host: env('DATABASE_HOST'),
-        port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'wasl'),
+        host: env('DATABASE_HOST','localhost'),
+        port: env.int('DATABASE_PORT',3306),
+        database: env('DATABASE_NAME','wasl'),
         user: env('DATABASE_USERNAME', 'root'),
         password: env('DATABASE_PASSWORD', 'root'),
         ssl: env.bool('DATABASE_SSL', false) && {
